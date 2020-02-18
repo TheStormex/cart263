b7c52475aa60c1ad9d7ddf229845ab42c2ed66f2
TheStormex <55155047+TheStormex@users.noreply.github.com>
2020-02-18 15:35:27 -0500

    music box

--- p5-project/js/script.js
+++ p5-project/js/script.js
@@ -2,36 +2,88 @@
 
 /*****************
 
-Title of Project
-Author Name
+Music box
+Che Tan
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Play a randomly generated song
 
 ******************/
 
-// preload()
-//
-// Description of preload
+const FREQUENCIES = [
+  440.00,
+  493.88,
+  554.37,
+  587.33,
+  659.25,
+  739.99,
+  830.61
+];
 
-function preload() {
+let pattern = [
+  'x',
+  '*',
+  '*',
+  'o',
+  'x',
+  '*',
+  'o',
+  'o',
+  'x',
+  '*',
+  'o',
+  '*',
+  'x',
+  'o',
+  '*',
+  'o'
+];
 
+let beat = 0;
+
+let kick = new Pizzicato.Sound({
+  source: 'file',
+  options: {path: './assets/sounds/kick.wav'}
+});
+let hihat = new Pizzicato.Sound({
+  source: 'file',
+  options: {path: './assets/sounds/hihat.wav'}
+});
+let snare = new Pizzicato.Sound({
+  source: 'file',
+  options: {path: './assets/sounds/snare.wav'}
+});
+let synth = new Pizzicato.Sound({
+  source: 'wave',
+  options: {
+    type: 'sine',
+    frequency: FREQUENCIES[0]
+  }
+})
+function setup() {}
+function draw() {}
+function playNote() {
+  synth.frequency = FREQUENCIES[Math.floor(Math.random() * FREQUENCIES.length)];
+  synth.play();
+}
+function mousePressed() {
+  setInterval(playNote, 500);
+  setInterval(playDrum, 125);
 }
 
-
-// setup()
-//
-// Description of setup
-
-function setup() {
-
-}
-
-
-// draw()
-//
-// Description of draw()
-
-function draw() {
-
+function playDrum() {
+  let symbols = pattern[beat];
+  if (symbols.includes('x')) {
+    kick.play();
+  }
+  if (symbols.includes('*')) {
+    hihat.play();
+  }
+  if (symbols.includes('o')) {
+    snare.play();
+  }
+  if (beat >= pattern.length-1) {
+    beat = 0;
+  } else {
+    beat++;
+  }
 }
