006c686eb346f71061fc67d3c0c70ead81cd671e
TheStormex <55155047+TheStormex@users.noreply.github.com>
2020-03-10 17:09:30 -0400

    A4: finished basic requirements

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -11,16 +11,19 @@ How to describe a sauce? Like this!
 $(document).ready(setup);
 
 let vowels =
-["a", "e", "i", "o", "u"];
+["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
+// the JSON data file
 let data;
 
+// Load the JSON file
 function setup() {
   $.getJSON("assets/data/data.json")
     .done(dataLoaded)
     .fail(dataError);
 }
 
+// Make a sentence and allow the user to click to make a new sentence
 function dataLoaded(theData) {
   data = theData;
   $('body').on("click", makeSentence);
@@ -33,6 +36,7 @@ function makeSentence() {
   let verb = "is";
   let a = "a";
   let a2 = "a";
+  let a3 = "a";
   let cat = getRandomElement(data.cats);
   let room = getRandomElement(data.rooms);
   let game = getRandomElement(data.games);
@@ -44,12 +48,18 @@ function makeSentence() {
     verb = "are";
   }
   for (var i = 0; i < vowels.length; i++) {
-    if (cat.charAt(0) === vowels[0]) {
+    if (cat.charAt(0) === vowels[i]) {
       a = "an";
     }
+    if (room.charAt(0) === vowels[i]) {
+      a2 = "an";
+    }
+    if (nationality.charAt(0) === vowels[i]) {
+      a3 = "an";
+    }
   }
-   let description = condiment + " " + verb + " like " + a + " " + cat + " in a " + room
-   + " playing " + game + " with " + a2 + " " + nationality + " " + plant + " dancing to " + genre + ".";
+   let description = condiment + " " + verb + " like " + a + " " + cat + " in " + a2 + " " + room
+   + " playing " + game + " with " + a3 + " " + nationality + " " + plant + " dancing to " + genre + ".";
    $('body').append(description)
 }
 
