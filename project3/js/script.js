bbcab7947103e10584d4394a4d9977638d77efcd
TheStormex <55155047+TheStormex@users.noreply.github.com>
2020-03-12 17:03:04 -0400

    P3: more variables, p5 libraries

--- project3/js/script.js
+++ project3/js/script.js
@@ -63,15 +63,35 @@ const V_AGENT_MAXHP = 50;
 const V_SERPENT_MAXHP = 60;
 const V_BOLT_ENERGY_TURN = 2;
 const V_NUTS_ENERGY_TURN = 2;
+const V_INSTRUCTIONS = [
+  "Reduce the 2 enemies' health to 0 through combat!",
+  "Click one of the two hackers to give commands in their menu!",
+  "Choose which of the two hackers to use as the front line fighter by clicking the Front line button in their menu! (Bolt is currently the frontline)",
+  "Spend energy to use support skills here in the planning phase and to use combat skills in the battle phase!",
+  "Using skills and dealing damage increases the ultimate meter!",
+  "Once it is full, that hacker can unleash either a powerful combat skill or support skill!",
+  "In combat, dodge enemy projectiles, shoot projectiles at them with the mouse and activate skills with shift, space and ctrl!",
+  "Each hacker gains energy at the beginning of turn, if it did not use a skill last turn, it gets more!",
+  "Switch between each hacker as the front line often to prevent one from becoming tired!",
+  "Good luck! The fate of humanity rests on your fingertips!",
+];
 
 // variables
 let boltHP = 20;
 let boltEnergy = 0;
+let boltUltCharge = 0;
+let boltFrontlineTurns = 0;
+let boltTired = false;
 let nutsHP = 30;
 let nutsEnergy = 0;
+let nutsUltChange = 0;
+let nutsFrontlineTurns = 0;
+let nutsTired = false;
+let frontline = "bolt";
 let agentHP = 50;
 let serpentHP = 60;
 
+let gameScreen;
 
 $(document).ready(start);
 
@@ -81,7 +101,8 @@ function start() {
 
 // p5 setup
 function setup() {
-
+  createCanvas(500, 500);
+  gameScreen = createGraphics(100, 100);
 }
 
 // p5 draw
